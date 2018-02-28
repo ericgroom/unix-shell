e64803f218718aad4c85561c8058804723d24941
Eric Groom <bcgroom@gmail.com>
2018-02-27 23:15:15 -0800

    implement rudimentary arg parsing

--- myshell.c
+++ myshell.c
@@ -26,11 +26,36 @@ void print_prompt() {
     printf("%s", PROMPT);
 }
 
+int strip(char *str, int size) {
+    for(int i = 0; i < size; i++) {
+        if (str[i] == '\n') {
+            str[i] = 0;
+            return i;
+        }
+    }
+}
+
+void parse(char *raw, int len, int *argc, char **argv) {
+    int i = 0;
+    for(char *token = strtok(raw, " "); token != NULL; token = strtok(NULL, " ")) {
+        argv[i] = token;
+        i++;
+    }
+    *argc = i;
+}
+
 void start_loop() {
     char buf[BUFFERSIZE];
+    int* myargc = malloc(4);
+    char** myargv = calloc(PIPECNTMAX*4, ARGVMAX);
     print_prompt();
     while(fgets(buf, BUFFERSIZE, stdin)) {
-        printf("contents: %s", buf);
+        int len = strip(buf, BUFFERSIZE);
+        parse(buf, len, myargc, myargv);
+        printf("myargc: %d\n", *myargc);
+        for(int i = 0; i < *myargc; i++) {
+            printf("contents[%d]: %s\n", i, myargv[i]);
+        }
         print_prompt();
     }
     printf("\n");
@@ -39,9 +64,6 @@ void start_loop() {
 int main(int* argc, char** argv)
 {
     start_loop();
-    
-    
-    
     
 return 0;
 }
